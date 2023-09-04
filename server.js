52678fa10d87f53227197ce87d0845894384cabf
Kirill Sheludchenko <kirill.shelydchenko@gmail.com>
2023-09-05 00:08:39 +0300

    add dockerfile

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get(
+app.get('/',
   (req, res, next) => {
     res.send('Hello from container')
   }
@@ -12,4 +12,3 @@ const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log('server is online')
 })
-
